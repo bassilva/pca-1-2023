3bdd7f776a0f64b7562a79160c397f3668e8ebd2
Lucas Dalle <lucasdalle@live.com>
2023-04-26 22:18:01 -0300

    adding stmts

--- src/whilelang/src/main.rs
+++ src/whilelang/src/main.rs
@@ -21,6 +21,19 @@ fn main() {
     let successful_parse_bexp2 = WhileParser::parse(Rule::BExp, "false");
     let successful_parse_bexp3 = WhileParser::parse(Rule::BExp, "not b");
 
+	let successful_parse_stmt1 = WhileParser::parse(Rule::assignment, "x := 9");
+	let successful_parse_stmt2 = WhileParser::parse(Rule::assignment, "x := read");
+	let successful_parse_stmt3 = WhileParser::parse(Rule::assignment, "x := a + b");
+	
+    let successful_parse_stmt4 = WhileParser::parse(Rule::conditional, "if x < a then { a := 4 } else { a := b }");
+	let successful_parse_stmt5 = WhileParser::parse(Rule::loopwhile, "while x !== a do { a := a - 1 }");
+    
+	let successful_parse_stmt6 = WhileParser::parse(Rule::print, "print \"hello world\"");
+    let successful_parse_stmt7 = WhileParser::parse(Rule::write, "write x");
+	let successful_parse_stmt8 = WhileParser::parse(Rule::read, "read");
+    let successful_parse_stmt9 = WhileParser::parse(Rule::skip, "skip");
+	
+
     println!("{:?}", successful_parse_num);
     println!("{:?}", successful_parse_var);
     println!("{:?}", successful_parse_lab);
@@ -32,4 +45,16 @@ fn main() {
     println!("{:?}", successful_parse_bexp1);
     println!("{:?}", successful_parse_bexp2);
     println!("{:?}", successful_parse_bexp3);
+	
+	println!("{:?}", successful_parse_stmt1);
+	println!("{:?}", successful_parse_stmt2);
+	println!("{:?}", successful_parse_stmt3);
+	
+	println!("{:?}", successful_parse_stmt4);
+	println!("{:?}", successful_parse_stmt5);
+	
+	println!("{:?}", successful_parse_stmt6);
+	println!("{:?}", successful_parse_stmt7);
+	println!("{:?}", successful_parse_stmt8);
+	println!("{:?}", successful_parse_stmt9);
 }
